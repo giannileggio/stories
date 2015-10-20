3b2ba48ba37c1e91c297b91f7a463d19633d4b90
Gianni Leggio <giannileggio@gmail.com>
2015-10-20 13:50:16 +0100

    Loading local json

--- app/routes/stories.js
+++ app/routes/stories.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return $.getJSON('stories.json', function(data){
-      console.log(data.stories[0].title);
-      return data.stories;
-    });
+    return $.getJSON('stories.json').then(
+      function(response) {
+        return response.stories;
+      }
+    );
   }
 });
